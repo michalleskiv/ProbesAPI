cdb5647debb05a70eb3b85cf6fb8a95c0fa05b15
Mike Leskiv <leskivmisha@gmail.com>
2020-11-24 17:36:44 +0100

    Small fixes

--- Probes/Startup.cs
+++ Probes/Startup.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using ProbesAPI.Middleware;
 using ProbesLib.Configurations.Models;
 using ProbesLib.Interfaces;
@@ -38,16 +37,6 @@ namespace ProbesAPI
                         Token = Configuration.GetSection("Token").Value,
                     }
                     ));
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "Probes API",
-                    Description = "Extends functionality of DrMax API"
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -68,17 +57,6 @@ namespace ProbesAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
-
-            app.UseSwagger(c =>
-            {
-                c.SerializeAsV2 = true;
-            });
-
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Probes API V1");
-                c.RoutePrefix = string.Empty;
             });
         }
     }
